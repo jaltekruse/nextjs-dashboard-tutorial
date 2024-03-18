c8e2da99da41c72edb8c19378af6e7f88952b259
Jason Altekruse <altekrusejason@gmail.com>
2024-03-18 11:53:38 -0500

    More stuff

--- app/dashboard/invoices/page.test.tsx
+++ app/dashboard/invoices/page.test.tsx
@@ -1,8 +1,25 @@
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Page from './page'
+import Page2 from '../(overview)/page'
+
+vi.mock('next/font/google', () => ({
+  Inter: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+  Lusitana: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+}))
  
 test('Page', () => {
-  render(<Page />)
+  // render(<Page />)
+  // expect(screen.getByRole('heading', { level: 1, name: 'Invoices Page' })).toBeDefined()
+
+  render(<Page2 />)
   expect(screen.getByRole('heading', { level: 1, name: 'Invoices Page' })).toBeDefined()
 })
